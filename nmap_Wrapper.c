4650fb538216021a8c9662d3a0fd139b857db213
M.ngugi <mngugi7@gmail.com>
2024-08-21 13:26:50 +0300

    nmap_Wrapper.c

--- nmap_Wrapper.c
+++ nmap_Wrapper.c
@@ -9,6 +9,20 @@ This code will do the following
 2. Creating a Python script that uses ctypes to interact with this library.
 3. Using the Nmap tool to execute specific scripts
 
+This C code provides a function run_nmap_script that takes a target IP/hostname
+and a script name, then constructs and executes the Nmap command using the system function.
+
 *****/
 
 
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void run_nmap_script(const char* target, const char* script ) {
+    char command[256];
+    snprintf(command, sizeof(command), "nmap --script=%s %s", script,target);
+    system(command);
+
+
+}

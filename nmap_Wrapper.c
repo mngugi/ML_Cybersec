8031a122516d0747fd2ae7a3a0112637a425f56a
M.ngugi <mngugi7@gmail.com>
2024-08-21 13:08:25 +0300

    nmap_Wrapper.c

---
+++ nmap_Wrapper.c
@@ -0,0 +1,14 @@
+
+/*****
+- nmap scripts that uses shared library through
+ctypes.
+
+This code will do the following
+
+1. Creating a C shared library that can execute Nmap commands.
+2. Creating a Python script that uses ctypes to interact with this library.
+3. Using the Nmap tool to execute specific scripts
+
+*****/
+
+

e1dd09a4114b31c39f28b0ac50a5d7f12fd51d97
M.ngugi <mngugi7@gmail.com>
2024-08-22 16:11:15 +0300

    monitor_router.c

--- monitor_router.c
+++ monitor_router.c
@@ -6,16 +6,38 @@
 #include <netinet/ip.h>
 #include <linux/if_ether.h>
 #include <unistd.h>
+#include <netdb.h>
 
-// Function to print the IP header details
+// Function to resolve IP address to a domain name
+void resolve_ip_to_name(char *ip_address, char *resolved_name, size_t len) {
+    struct sockaddr_in sa;
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr(ip_address);
+
+    if (getnameinfo((struct sockaddr*)&sa, sizeof(sa), resolved_name, len, NULL, 0, 0) != 0) {
+        strncpy(resolved_name, "Unknown", len);
+    }
+}
+
+// Function to print the IP header details with resolved names
 void print_ip_header(struct iphdr *ip_header) {
     struct in_addr src, dest;
     src.s_addr = ip_header->saddr;
     dest.s_addr = ip_header->daddr;
 
+    char src_ip[INET_ADDRSTRLEN];
+    char dest_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(src.s_addr), src_ip, INET_ADDRSTRLEN);
+    inet_ntop(AF_INET, &(dest.s_addr), dest_ip, INET_ADDRSTRLEN);
+
+    char src_name[NI_MAXHOST];
+    char dest_name[NI_MAXHOST];
+    resolve_ip_to_name(src_ip, src_name, sizeof(src_name));
+    resolve_ip_to_name(dest_ip, dest_name, sizeof(dest_name));
+
     printf("IP Header Information:\n");
-    printf("   |-Source IP        : %s\n", inet_ntoa(src));
-    printf("   |-Destination IP   : %s\n", inet_ntoa(dest));
+    printf("   |-Source IP        : %s (%s)\n", src_ip, src_name);
+    printf("   |-Destination IP   : %s (%s)\n", dest_ip, dest_name);
     printf("   |-Protocol         : %d\n", (unsigned int)ip_header->protocol);
     printf("   |-Packet Length    : %d\n", ntohs(ip_header->tot_len));
 }

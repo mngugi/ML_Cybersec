f0c8c3ae106805b03787e022d7f8a795a2758062
M.Ngugi <mngugi7@gmail.com>
2025-01-10 02:05:11 +0300

    Update CrytographicSequence.py

--- CrytographicSequence.py
+++ CrytographicSequence.py
@@ -1,5 +1,13 @@
 import secrets
 import string
+import pyfiglet # type: ignore
+
+def display_title(title):
+    ascii_art = pyfiglet.figlet_format(title)
+    print(ascii_art)
+
+if __name__ == "__main__":
+    display_title("Generated cryptographic sequence")
 
 def generate_cryptographic_sequence(length=32):
     # Define the character set

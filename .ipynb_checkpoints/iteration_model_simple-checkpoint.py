1309415fa42fe9f7177c56f44a4d83955b8b91a0
M.Ngugi <mngugi7@gmail.com>
2025-01-11 21:18:54 +0300

    iteration_model_simple.py

--- .ipynb_checkpoints/iteration_model_simple-checkpoint.py
+++ .ipynb_checkpoints/iteration_model_simple-checkpoint.py
@@ -1,12 +1,5 @@
 # Quadratic iteration model: X_{n+1} = X_n^2
 # Initial value
-def display_title(title):
-    ascii_art = pyfiglet.figlet_format(title)
-    print(ascii_art)
-
-if __name__ == "__main__":
-    display_title("Quadratic iteration model")
-
 X_0 = 0.1  # Initial vulnerability level
 iterations = 10  # Number of iterations
 results = [X_0]  # List to store results
